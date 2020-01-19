7ef182151b233acf8b009eecf914da4e9bc85e48
catatsuy <catatsuy@catatsuy.org>
2020-01-19 21:53:59 +0900

    30min

--- config/cache.go
+++ config/cache.go
@@ -46,7 +46,7 @@ func LoadCache() (cache Cache, exist bool, err error) {
 		return Cache{}, false, err
 	}
 	now := time.Now()
-	if !fi.ModTime().Add(time.Hour).After(now) {
+	if !fi.ModTime().Add(30 * time.Minute).After(now) {
 		return Cache{}, false, nil
 	}
 
